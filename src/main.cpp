f6ac789236d747fa7de9d8cb57fb4ffd09361f62
kiands <794583439@qq.com>
2023-04-22 21:02:03 -0400

    v0.8

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,46 @@
 // #include "drivers/stm32f429i_discovery_lcd.h"
 #include "drivers/LCD_DISCO_F429ZI.h"
 #include "drivers/stm32f429i_discovery_ts.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+volatile int TouchCount = 0;
+
+struct GyroData {
+    float x;
+    float y;
+    float z;
+
+    GyroData(float x, float y, float z) : x(x), y(y), z(z) {}
+};
+
+using GyroDataSet = std::vector<GyroData>;
+
+GyroDataSet data1;
+GyroDataSet data2;
+
+float euclideanDistance(const GyroData& a, const GyroData& b) {
+    return std::sqrt(std::pow(a.x - b.x, 2) + std::pow(a.y - b.y, 2) + std::pow(a.z - b.z, 2));
+}
+
+float calculateDTWDistance(const GyroDataSet& a, const GyroDataSet& b) {
+    int n = a.size();
+    int m = b.size();
+    std::vector<std::vector<float>> dtw(n + 1, std::vector<float>(m + 1, std::numeric_limits<float>::infinity()));
+    dtw[0][0] = 0;
+
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+                        float cost = euclideanDistance(a[i - 1], b[j - 1]);
+            dtw[i][j] = cost + std::min({dtw[i - 1][j], dtw[i][j - 1], dtw[i - 1][j - 1]});
+        }
+    }
+
+    return dtw[n][m];
+}
+
+float similarity = 0.00;
 
 volatile bool isRecording = false;
 
@@ -40,7 +80,7 @@ LCD_DISCO_F429ZI lcd;
 // 初始化LED引脚
 DigitalOut led(LED1);
 
-int gyro_thread() {
+GyroData gyro_thread() {
     // Setup the spi for 8 bit data, high steady state clock,
     // second edge capture, with a 1MHz clock rate
     spi.format(8,3);
@@ -55,45 +95,51 @@ int gyro_thread() {
     write_buf[1]=CTRL_REG4_CONFIG;
     spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
     flags.wait_all(SPI_FLAG);
-  
-    // if (isRecording) {
-    if (true) {
-        volatile int16_t raw_gx,raw_gy,raw_gz;
-        volatile float gx, gy, gz;
-        //reading the status register. bit 4 of the status register
-        //is 1 when a new set of samples is ready
-        write_buf[0]=0x27 | 0x80;
-        do{
-          spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
-          flags.wait_all(SPI_FLAG);
-        }while((read_buf[1]&0b0000'1000)==0);
 
-        //prepare the write buffer to trigger a sequential read
-        write_buf[0]=OUT_X_L|0x80|0x40;
+    volatile int16_t raw_gx,raw_gy,raw_gz;
+    volatile float gx, gy, gz;
+    //reading the status register. bit 4 of the status register
+    //is 1 when a new set of samples is ready
+    write_buf[0]=0x27 | 0x80;
+    do{
+    spi.transfer(write_buf,2,read_buf,2,spi_cb,SPI_EVENT_COMPLETE );
+    flags.wait_all(SPI_FLAG);
+    }while((read_buf[1]&0b0000'1000)==0);
 
-        //start sequential sample reading
-        spi.transfer(write_buf,7,read_buf,8,spi_cb,SPI_EVENT_COMPLETE );
-        flags.wait_all(SPI_FLAG);
+    //prepare the write buffer to trigger a sequential read
+    write_buf[0]=OUT_X_L|0x80|0x40;
 
-        //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
-        //Put the high and low bytes in the correct order lowB,Highb -> HighB,LowB
-        raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
-        raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
-        raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
+    //start sequential sample reading
+    spi.transfer(write_buf,7,read_buf,8,spi_cb,SPI_EVENT_COMPLETE );
+    flags.wait_all(SPI_FLAG);
 
-        // printf("RAW|\tgx: %d \t gy: %d \t gz: %d\n",raw_gx,raw_gy,raw_gz);
+    //read_buf after transfer: garbage byte, gx_low,gx_high,gy_low,gy_high,gz_low,gz_high
+    //Put the high and low bytes in the correct order lowB,Highb -> HighB,LowB
+    raw_gx=( ( (uint16_t)read_buf[2] ) <<8 ) | ( (uint16_t)read_buf[1] );
+    raw_gy=( ( (uint16_t)read_buf[4] ) <<8 ) | ( (uint16_t)read_buf[3] );
+    raw_gz=( ( (uint16_t)read_buf[6] ) <<8 ) | ( (uint16_t)read_buf[5] );
 
-        gx=((float)raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
-        gy=((float)raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
-        gz=((float)raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
+    // printf("RAW|\tgx: %d \t gy: %d \t gz: %d\n",raw_gx,raw_gy,raw_gz);
 
-        return raw_gz;
-    }
+    gx=((float)raw_gx)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
+    gy=((float)raw_gy)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
+    gz=((float)raw_gz)*(17.5f*0.017453292519943295769236907684886f / 1000.0f);
+    GyroData gyroSample(gx, gy, gz);
+    // data2.push_back(gyroSample);
     // ThisThread::sleep_for(100);
+    return gyroSample;
+}
+
+bool isTouchEnded() {
+    TS_StateTypeDef currentState;
+    ThisThread::sleep_for(50ms); // 等待一段时间，以确保手指离开触摸屏
+    BSP_TS_GetState(&currentState);
+    return !currentState.TouchDetected;
 }
 
 int main() {
-    int16_t raw_gz = gyro_thread();
+
+    // int16_t raw_gz = gyro_thread();
     // 初始化LCD
     lcd.Clear(LCD_COLOR_WHITE);
     lcd.SetBackColor(LCD_COLOR_WHITE);
@@ -101,7 +147,8 @@ int main() {
     lcd.SetFont(&Font24);
 
     // 转换整数和浮点数为字符串
-    char buffer[32];
+    char buffer1[32];
+    char buffer2[32];
     // sprintf(buffer, "Integer: %d", integer_value);
 
     // 在屏幕上显示整数
@@ -110,11 +157,11 @@ int main() {
     // lcd.DisplayStringAt(x, y, (uint8_t *)buffer, LEFT_MODE);
     // 转换浮点数为字符串（保留两位小数）
     // 小数：%2.f
-    sprintf(buffer, "Integer: %d", raw_gz);
+    // sprintf(buffer, "Float: %2.f", similarity);
 
     // 在屏幕上显示浮点数
-    y += lcd.GetFont()->Height;
-    lcd.DisplayStringAt(x, y, (uint8_t *)buffer, LEFT_MODE);
+    // y += lcd.GetFont()->Height;
+    // lcd.DisplayStringAt(x, y, (uint8_t *)buffer, LEFT_MODE);
 
     // Gyro thread
     // 创建线程对象
@@ -128,18 +175,34 @@ int main() {
     BSP_TS_Init(lcd.GetXSize(), lcd.GetYSize());
 
     while (1) {
+        sprintf(buffer1, "Integer: %d", TouchCount);
+        sprintf(buffer2, "Float: %2.f", similarity);
         BSP_TS_GetState(&ts_state);
 
-        if (ts_state.TouchDetected) {
+        if (ts_state.TouchDetected && isTouchEnded()) {
             isRecording = !isRecording; // Switcher
+            TouchCount = TouchCount + 1;
         }
 
         if (isRecording) {
             led = 1;
+            if (TouchCount == 1) {
+                data1.push_back(gyro_thread());
+            } else if (TouchCount == 3) {
+                data2.push_back(gyro_thread());
+            }
+            ThisThread::sleep_for(20);
         } else {
             led = 0;
         }
 
+        if (TouchCount == 4) {
+            similarity = calculateDTWDistance(data1, data2);
+        }
+
         // ThisThread::sleep_for(1s);
+        lcd.DisplayStringAt(x, 10, (uint8_t *)buffer1, LEFT_MODE);
+        lcd.DisplayStringAt(x, 35, (uint8_t *)buffer2, LEFT_MODE);
     }
+
 }
